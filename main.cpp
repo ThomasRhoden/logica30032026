8a3589eebea74a9238fee258e9a9eb4184dc73f5
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-30 19:50:32 -0300

    Primeiro commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+
+int main(){
+
+    std::string diaDaSemana;
+
+    std::cout<<"Que dia é hoje: "<<std::endl;
+    std::cin>>diaDaSemana;
+
+    std::cout<<"Hoje é "<<diaDaSemana<<"!!"<<std::endl;
+
+
+    return 0;
+}
