bd7e713345a9db21d4f6546db6881082898d191c
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-30 20:10:37 -0300

    Commit 2

---
+++ main2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+int main(){
+
+    int a;
+    int b;
+    //int a, b;
+
+    std::cout<<"Digite um valor para A: "<<std::endl;
+    std::cin>>a;
+
+    std::cout<<"Digite um valor para B: "<<std::endl;
+    std::cin>>b;
+
+    std::cout<<"A = "<<a<<" e B = "<<b<<std::endl;
+    
+
+    return 0;
+}
