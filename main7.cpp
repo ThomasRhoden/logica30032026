6cac163c5efaeb762b5b83d6b4c781e71c26ee0a
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-30 21:59:11 -0300

    commit 7,1

--- main7.cpp
+++ main7.cpp
@@ -3,10 +3,12 @@
 int main(){
 
     int a = 10;
-    int b = 6;
+    int b = 65;
 
     std::cout<<((a > b) && (a + b) >= 16)<<std::endl;
-
-
+    // ambas condições são verdade (&& ambas)
+    std::cout<<((a > b) || (a + b) >= 16)<<std::endl;
+    // basta uma condição sendo verdade (|| ou)
+    
     return 0;
 }
