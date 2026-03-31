af5c8d46d2e3837bf123cce5b6de655c7f7f2c90
ThomasRhoden <rhodenthomas@gmail.com>
2026-03-30 21:06:36 -0300

    commit 3

---
+++ main3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+
+    int soma1 = 100 + 50;
+    int soma2 = soma1 + 50;
+    int soma3 = soma2 + 100;
+
+    std::cout<<soma2<<std::endl;
+    std::cout<<soma3<<std::endl;
+
+
+    return 0;
+}
